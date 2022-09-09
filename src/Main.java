78cc5895f6b776fe9b8271faa941c3b9563b2e90
ZooWonSEO <seozoo123@gmail.com>
2022-09-09 14:16:33 +0900

    add content

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,13 @@ import java.util.*;
 public class Main {
     public static void main(String args[]){
 
+        ArrayList<String> list = new ArrayList<>();
+        list.add("A");
+        list.add("B");
+        list.add("C");
+
+        String arr[] = list.toArray(new String[list.size()]);
+        System.out.println(arr);
 
     }
 }
