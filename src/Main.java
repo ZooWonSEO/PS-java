6f2345bbf1ba2349f9826f93415c3aef1fe5c2ab
ZooWonSEO <seozoo123@gmail.com>
2022-09-09 14:09:39 +0900

    add content

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,6 @@ import java.util.*;
 public class Main {
     public static void main(String args[]){
 
-        System.out.println(Math.max(10,2));
-        System.out.println(Math.min(10,2));
 
     }
 }
