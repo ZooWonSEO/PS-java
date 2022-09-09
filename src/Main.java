e1ce27464f2c9a0ea54ae201d662f826770bc7d4
ZooWonSEO <seozoo123@gmail.com>
2022-09-09 16:30:04 +0900

    add content

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,19 @@ import java.util.*;
 public class Main {
     public static void main(String args[]){
 
-        ArrayList<Integer> arr = new ArrayList<>();
+        String str = "Hello World!!";
+        System.out.println(str);
 
-        
+        char[] arr = str.toCharArray();
+        System.out.println(arr);
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<arr.length; i++){
+            sb.append(arr[i]);
+        }
+        System.out.println(sb);
+        System.out.println(sb.toString());
+
 
     }
 }
