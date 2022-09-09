7185c4e07c0d45d2fd7c57b8008acb3921abee46
ZooWonSEO <seozoo123@gmail.com>
2022-09-09 17:44:06 +0900

    add content

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,23 @@ import java.util.*;
 public class Main {
     public static void main(String args[]){
 
-        String str = "Hello World!!";
-        System.out.println(str);
+        // 선언
+        Set<Integer> set = new HashSet<>();
 
-        char[] arr = str.toCharArray();
-        System.out.println(arr);
+// 데이터 삽입
+        set.add(1);
+        set.add(3);
+        set.add(4);
+        set.add(2);
 
-        StringBuilder sb = new StringBuilder();
-        for(int i=0; i<arr.length; i++){
-            sb.append(arr[i]);
+// 데이터 삭제
+        set.remove(2);
+
+// 데이터 순회
+        Iterator<Integer> it = set.iterator();
+        while(it.hasNext()) {
+            System.out.print(it.next() + " ");
         }
-        System.out.println(sb);
-        System.out.println(sb.toString());
-
 
     }
 }
