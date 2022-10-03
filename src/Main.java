e06fd4b35ee42f7209ae051c19d853b5cfb16a84
ZooWonSEO <seozoo123@gmail.com>
2022-10-03 20:47:26 +0900

    add

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,22 @@ import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main {
-
     public static void main(String args[]){
 
         Scanner sc = new Scanner(System.in);
 
+        String str = sc.nextLine();
+        int m = sc.nextInt();
+
+        char[] arr= str.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<arr.length; i++){
+            sb.append(arr[i]);
+        }
 
 
-        System.out.println(answer);
+
+
+
     }
 }
