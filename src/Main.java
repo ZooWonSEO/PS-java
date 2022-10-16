144bbb0534c64ca4cd0a30c475c6895b7de882ea
ZooWonSEO <seozoo123@gmail.com>
2022-10-16 21:36:36 +0900

    add

--- src/Main.java
+++ src/Main.java
@@ -7,43 +7,6 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        sc.nextLine();
-        String s = sc.nextLine();
-        char arr[] = s.toCharArray();
-
-        int row = 1;
-        int col = 1;
-
-        for(int i=0; i<arr.length; i++) {
-
-            int r = 0;
-            int c = 0;
-
-            if(arr[i] == 'L') {
-                c = -1;
-            }
-
-            else if(arr[i] == 'R') {
-                c = 1;
-            }
-
-            else if(arr[i] == 'U') {
-                r = -1;
-            }
-
-            else if(arr[i] == 'D') {
-                r = 1;
-            }
-
-            if(row+r > n || col+c > n || row+r < 1 || col+c < 1)
-                continue;
-
-            row = row + r;
-            col = col + c;
-        }
-
-        System.out.print(row + " " + col);
 
     }
 }
