cc25e1dd499924e8b928c6e82d53d69b4d779cd3
ZooWonSEO <seozoo123@gmail.com>
2022-10-17 22:13:19 +0900

    add

--- src/Main.java
+++ src/Main.java
@@ -7,26 +7,8 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int[][] map = new int[n][m];
-
-        int px = sc.nextInt();
-        int py = sc.nextInt();
-        int d = sc.nextInt();
-
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                map[i][j] = sc.nextInt();
-            }
-        }
-
-        int[] x = {-1, 0, 1, 0};
-        int[] y = {0, 1, 0, -1};
 
         int answer = 0;
-
-
 
         System.out.println(answer);
     }
