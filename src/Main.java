34d4d5f6ef1b61ea57579cc34519a91c97fa2f21
ZooWonSEO <seozoo123@gmail.com>
2022-10-18 21:54:43 +0900

    add

--- src/Main.java
+++ src/Main.java
@@ -7,26 +7,9 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        String s = sc.nextLine();
 
-        int row = s.charAt(0) - 'a';
-        int col = s.charAt(1) - '1';
 
-        int r[] = {-1, -2, -1, -2, 1, 2, 1, 2};
-        int c[] = {-2, -1, 2, 1, -2, -1, 2, 1};
 
-        int answer = 0;
 
-        for(int i=0; i<8; i++) {
-            int nr = row + r[i];
-            int nc = col + c[i];
-
-            if(nr<0 || nc<0 || nr>7 || nc>7)
-                continue;
-
-            answer++;
-        }
-
-        System.out.println(answer);
     }
 }
