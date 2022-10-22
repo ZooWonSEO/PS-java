1b813239301dcd42e8ae3e54605478dcb6fabcb5
ZooWonSEO <seozoo123@gmail.com>
2022-10-22 13:17:09 +0900

    add

--- nadongbin/2_Implementation/Q7_럭키스트레이트.java
+++ nadongbin/2_Implementation/Q7_럭키스트레이트.java
@@ -21,3 +21,37 @@ LUCKY
 READY
 
 */
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        int left = 0;
+        int right = 0;
+
+        for(int i=0; i<str.length(); i++) {
+
+            if(i < str.length()/2) {
+                left = left + (str.charAt(i) - '0');
+            }
+            else{
+                right = right + (str.charAt(i) - '0');
+            }
+        }
+
+        if(left == right) {
+            System.out.println("LUCKY");
+        }
+        else {
+            System.out.println("READY");
+        }
+    }
+}
+
--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,26 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
+        String str = sc.nextLine();
 
+        int left = 0;
+        int right = 0;
 
+        for(int i=0; i<str.length(); i++) {
 
+            if(i < str.length()/2) {
+                left = left + (str.charAt(i) - '0');
+            }
+            else{
+                right = right + (str.charAt(i) - '0');
+            }
+        }
 
+        if(left == right) {
+            System.out.println("LUCKY");
+        }
+        else {
+            System.out.println("READY");
+        }
     }
 }
