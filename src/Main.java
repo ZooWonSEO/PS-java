72234eafa254fcf6172d963c6df452b06311f382
ZooWonSEO <seozoo123@gmail.com>
2022-10-22 15:23:41 +0900

    add

--- src/Main.java
+++ src/Main.java
@@ -5,21 +5,7 @@ public class Main {
 
     public static void main(String args[]){
 
-        Stack<Integer> st = new Stack<>();
+        
 
-        st.add(5);
-        st.add(2);
-        st.add(3);
-        st.add(7);
-        st.pop();
-
-        st.add(1);
-        st.add(4);
-        st.pop();
-
-        while(st.isEmpty()==false) {
-            System.out.print(st.peek() + " ");
-            st.pop();
-        }
     }
 }
