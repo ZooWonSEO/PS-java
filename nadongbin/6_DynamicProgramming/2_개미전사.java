c3d0b79fea4ad6d42728510e6d72650feafb24e4
ZooWonSEO <seozoo123@gmail.com>
2022-11-27 19:44:07 +0900

    add problems

--- nadongbin/6_DynamicProgramming/2_개미전사.java
+++ nadongbin/6_DynamicProgramming/2_개미전사.java
@@ -21,3 +21,30 @@
 8
 
 */
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int[] dp = new int[n];
+        dp[0] = arr[0];
+        dp[1] = arr[1];
+
+        for(int i=3; i<n; i++) {
+            dp[i] = Math.max(dp[i-1], dp[i-2]+arr[i]);
+        }
+
+        int answer = dp[n-1];
+        System.out.print(answer);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -12,9 +12,15 @@ public class Main {
             arr[i] = sc.nextInt();
         }
 
+        int[] dp = new int[n];
+        dp[0] = arr[0];
+        dp[1] = arr[1];
 
+        for(int i=3; i<n; i++) {
+            dp[i] = Math.max(dp[i-1], dp[i-2]+arr[i]);
+        }
 
-
-
+        int answer = dp[n-1];
+        System.out.print(answer);
     }
 }
