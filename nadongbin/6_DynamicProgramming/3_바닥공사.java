4b8f88a358a55ad27ec7057b9f0457530f5a3f66
ZooWonSEO <seozoo123@gmail.com>
2022-11-27 20:11:40 +0900

    add problems

--- nadongbin/6_DynamicProgramming/3_바닥공사.java
+++ nadongbin/6_DynamicProgramming/3_바닥공사.java
@@ -15,3 +15,26 @@
 5
 
 */
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] dp = new int[n+1];
+
+        dp[1] = 1;
+        dp[2] = 3;
+        dp[3] = 5;
+
+        for(int i=4; i<=n; i++) {
+            dp[i] = (dp[i-1] + (2*dp[i-2])) % 796796;
+        }
+
+        int answer = dp[n];
+        System.out.print(answer);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -7,20 +7,17 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        int[] arr = new int[n];
-        for(int i=0; i<n; i++) {
-            arr[i] = sc.nextInt();
+        int[] dp = new int[n+1];
+
+        dp[1] = 1;
+        dp[2] = 3;
+        dp[3] = 5;
+
+        for(int i=4; i<=n; i++) {
+            dp[i] = (dp[i-1] + (2*dp[i-2])) % 796796;
         }
 
-        int[] dp = new int[n];
-        dp[0] = arr[0];
-        dp[1] = arr[1];
-
-        for(int i=3; i<n; i++) {
-            dp[i] = Math.max(dp[i-1], dp[i-2]+arr[i]);
-        }
-
-        int answer = dp[n-1];
+        int answer = dp[n];
         System.out.print(answer);
     }
 }
